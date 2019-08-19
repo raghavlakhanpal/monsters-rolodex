5862a0b6579dbc168fdc79818b9dcf0a73efa975
raghavlakhanpal <raghavlakhanpal57@gmail.com>
2019-08-19 15:55:22 +0530

    Updates

---
+++ precache-manifest.a6a775a8781db8445ef895ab815c35ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604f39e0b4dc4dce684e96f79686ffad",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3c628e6c5750c30ee846",
+    "url": "/monsters-rolodex/static/css/main.7753f4c1.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "3c628e6c5750c30ee846",
+    "url": "/monsters-rolodex/static/js/main.fb18f211.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
